1c2d4743cc82847594712b9d4be6f1a171fc45c3
Antoine Dangeard <antoine.dangeard@mail.mcgill.ca>
2021-11-05 22:38:18 -0400

    cleaned up project, made scoring function and made data saving and loading to a file

--- BBModel.py
+++ BBModel.py
@@ -2,87 +2,21 @@ import os, sys
 import numpy as np
 import pandas as pd
 import sklearn.linear_model as skm
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+import GenerateGameData
 
-gamesPerSeason = 162
-numStats = 16
-polynomialDegree = 10
-training_set = "data/TRAINING_1990-2020.txt"
-test_set = "data/TEST_1990-2020.txt"
-valid_set = "data/VALIDATION_1990-2020.txt"
+# hyperparameters
 learning_rate = 0.00001
 epsilon = 0.001
 lambda_value = 0.00001
-max_samples = 1000
-
-def getTrainingData(set):
-    dataSet = open(set).readlines()
-
-    stats = np.zeros( (len(dataSet[:max_samples]), 2*numStats*polynomialDegree) ) #first 16*5 stats are polynimalized stats of home team, last 16*5 are visiting team
-    scores = np.zeros(len(dataSet[:max_samples]))
-    mins, maxs = findMinMaxFromStatSheet(open("data/mlbStats2021.txt").readlines())
-
-    for i in range(len(dataSet[:max_samples])):
-        game = dataSet[:max_samples][i]
-        year = int(game.split(",")[0][:4])
-        homeTeam = game.split(",")[1]
-        visitingTeam = game.split(",")[2]
-        home = 0
-        visiting = 0
-        scoreDifference = int(game.split(",")[3])
-        gameWorthHome = int(game.split(",")[4])
-        gameWorthVisitors = int(game.split(",")[4])
-        for p in range(polynomialDegree):
-            stats[i][p] = (gameWorthHome/gamesPerSeason)**p
-            stats[i][polynomialDegree+p] = (gameWorthVisitors/gamesPerSeason)**p
-        statSheet = open("data/mlbStats" + str(year) + ".txt").readlines()
-        for j in range(len(statSheet)):
-            teamStats = statSheet[j]
-            if teamStats.split(",")[0] == homeTeam and home != 1:
-                home = 1
-                n = 2*polynomialDegree
-                for s in range(numStats-1): #removing one because the gameWorth stat has already been assigned
-                    stdStat = float(standardize(float(teamStats.split(",")[3:][s]), mins[s], maxs[s]))
-                    for p in range(polynomialDegree):
-                        stats[i][n] = stdStat**p
-                        n += 1
-                        #first half of stats[i] is home team stats and their polynomials
-            elif teamStats.split(",")[0] == visitingTeam and visiting != 1:
-                visiting = 1
-                n = (numStats+1)*polynomialDegree
-                for s in range(numStats-1):
-                    stdStat = float(standardize(float(teamStats.split(",")[3:][s]), mins[s], maxs[s]))
-                    for p in range(polynomialDegree):
-                        stats[i][n] = stdStat**p
-                        n += 1
-                        #second half of stats[i] is visiting team stats and their polynomials 
-        if visiting == 0: print(str(year) + "incomplete stats: " + visitingTeam)
-        if home == 0: print(str(year) + " incomplete stats: " + homeTeam)
-        scores[i] = scoreDifference
-        if (i%300 == 0):
-            print("%.2f" % float((i/len(dataSet[:max_samples])*100)) + "%")
-
-    return stats, scores
+numStats = GenerateGameData.numStats
 
 def predictScore(w, s): # w is (2*numStats*polyDegree)x1 matrix, s is (2 * numStats * polyDegree)x1 matrix, output is array of predicted scores (one per game)
     predictedScore = [0]*len(s)
     for i in range(len(s)):
         predictedScore[i] = s[i].dot(w)
     return predictedScore
-
-def findMinMaxFromStatSheet(s):
-    maxs = [0]*(numStats-1)
-    mins = [100000]*(numStats-1)
-
-    for i in range(len(s[1:])):
-        line = s[1:][i]
-        for j in range(len(line.split(",")[3:])):
-            maxs[j] = max(float(line.split(",")[3:][j]), maxs[j])
-            mins[j] = min(float(line.split(",")[3:][j]), mins[j])
-
-    return mins, maxs
-
-def standardize(stat, min, max):
-    return float(stat-min)/float(max-min) #scaling each feature between 0 and 1
 
 # function that calculates the gradient
 def calculate_regularized_grad(X_poly, y, W, lambda_value):
@@ -109,7 +43,6 @@ def calculate_regularized_grad(X_poly, y, W, lambda_value):
 
 # function that caculates the change in W
 def calculate_dist(W_prev, W_cur):
-    print(W_prev)
     print(W_cur)
     return np.sqrt(np.sum((W_cur - W_prev)**2), dtype=np.float64)
 
@@ -122,19 +55,15 @@ def train_regularized_polynomial_regression(X_poly, y, W, learning_rate, epsilon
 
       W_prev = W.copy()
 
-      ### YOUR CODE HERE - update W[j] using the given learning_rate
-      # Hint: This should be the same as your implementation from Q2
-
       for j in range(W.shape[0]): 
         W[j] -= learning_rate*dW[j]
-
-      ### ------------------------------
 
       diff = calculate_dist(W_prev, W)
       if (diff < epsilon):
           break
 
       epoch_count +=1
+
       # print train error every 50 iterations
       if verbose:
         if epoch_count % 100 == 0:
@@ -143,23 +72,36 @@ def train_regularized_polynomial_regression(X_poly, y, W, learning_rate, epsilon
 
   print('Training complete.')
   return W
- 
-###################
 
-weights = np.zeros(2*numStats*polynomialDegree)
-training_x, training_y = getTrainingData(training_set)
-test_x, test_y = getTrainingData(test_set)
+#function that prints the score of a model's prediction on a stat set
+def score(pred, true):
+    num_samples = len(pred)
+    totalSquaredError = 0.0
+    correct_outcomes = 0.0
+    for i in range(len(pred)):
+        if pred[i]*true[i] > 0: correct_outcomes += 1
+        totalSquaredError += (pred[i]-true[i])**2
 
-reg = skm.LinearRegression().fit(training_x, training_y)
-print(reg.score(test_x, test_y))
-reg = skm.Ridge().fit(training_x, training_y)
-print(reg.score(test_x, test_y))
+    print("===== SCORE =====")
+    print("Mean squared error: " + str(totalSquaredError/num_samples))
+    print("Model predicted outcome correctly " + str(correct_outcomes) + " times out of " + str(num_samples) + " games. (%.2f" % float(correct_outcomes/num_samples*100.0) + "% Accuracy)")
+    print("=================")
 
+################### TESTING CODE
 
+training_x, training_y = GenerateGameData.loadFromFile("data/TrainingData1990-2020.txt")
+test_x, test_y = GenerateGameData.loadFromFile("data/TestData1990-2020.txt")
 
+model = make_pipeline(StandardScaler(), skm.LinearRegression(n_jobs=-1))
+model.fit(training_x, training_y)
+
+predictedScores = model.predict(test_x)
+score(predictedScores, test_y)
 
 
 exit()
+
+
 valid_x, valid_y = getTrainingData(valid_set)
 W = train_regularized_polynomial_regression(training_x, training_y, weights, learning_rate, epsilon, lambda_value)
 #calculate squared error on training set
--- GenerateGameData.py
+++ GenerateGameData.py
@@ -1,10 +1,16 @@
 import os, sys
 import random
+import numpy as np
 
-#Create a variable for the file name
+max_samples = -1 #maximum number of games we use for training (negative for infinite)
+polynomialDegree = 10
+gamesPerSeason = 162
+numStats = 15 + 2 #+2 for the game worth stat this is temporary
+startYear = 1990
+endYear = 2020
+fieldSeperator = "?"
 
-gameData = ""
-
+#dictionary to convert team code into team name
 teams = dict({"ATL": "Atlanta Braves",
     "LAN": "Los Angeles Dodgers",
     "NYN": "New York Mets",
@@ -52,59 +58,228 @@ teams = dict({"ATL": "Atlanta Braves",
     "PT1": "Pittsburgh Pirates",
     "PIT": "Pittsburgh Pirates" })
 
-startYear = 1990
-endYear = 2020
-
-for y in range(endYear-startYear+1):
-    filename = "records/GL" + str(startYear+y) + ".TXT"
-    print(filename)
-    infile = open(filename, 'r')
-    games = infile.read().split("\n")
-
-    for i in range(len(games)):
-        cur = games[i].split(',')
-        if len(cur) < 11: continue
-        game = ""
-        date = cur[0].strip('"')
-        visitorScore = int( cur[9].strip('"') )
-        homeScore = int( cur[10].strip('"') )
-        homeTeam = teams.get( cur[6].strip('"'), "" )
-        visitingTeam = teams.get( cur[3].strip('"'), "" )
-        gamesPlayedHome = int(cur[8].strip('"'))
-        gamesPlayedVisitors = int(cur[5].strip('"'))
-        if (visitingTeam != "" and homeTeam != ""):
-            game += date + "," + homeTeam + "," + visitingTeam + "," + str(homeScore-visitorScore) + "," + str(gamesPlayedHome) + "," + str(gamesPlayedVisitors)
-            gameData += game + "\n"
-    infile.close()
-
-#open text file
-newFileName = os.getcwd() + "/data/ALLGAMES_" + str(startYear) + "-" + str(endYear) + ".txt"
-os.remove(newFileName)
-output = open(newFileName, "w")
- 
-#write string to file
-output.write("date,homeTeam,visitingTeam,scoreDifference,gamesPlayedInSeason\n")
-output.write(gameData)
-output.close()
-
-#shuffle lines of file randomly
-lines = open(newFileName).readlines()[1:]
-random.shuffle(lines)
-
-
-#split our shuffled data into training, test, and validations data sets
-trainingFileName = os.getcwd() + "/data/TRAINING_" + str(startYear) + "-" + str(endYear) + ".txt"
-testFileName = os.getcwd() + "/data/TEST_" + str(startYear) + "-" + str(endYear) + ".txt"
-validationFileName = os.getcwd() + "/data/VALIDATION_" + str(startYear) + "-" + str(endYear) + ".txt"
-
-try:
-    os.remove(trainingFileName)
-    os.remove(testFileName)
-    os.remove(validationFileName)
-except:
-    a=0
+# function to turn a dataset of baseball games into a file containing stats and score differences
+# for every game in our data
+def createTrainingData(set):
     
-open(trainingFileName, 'w').writelines(lines[:len(lines)//2]) #write half of data to training
-open(testFileName, 'w').writelines(lines[3*len(lines)//4:]) #write a quarter of data to test 
-open(validationFileName, 'w').writelines(lines[len(lines)//2:-len(lines)//4]) #write a quarter of data to validation
+    if max_samples < 0:
+        data_selection = set
+    else:
+        data_selection = set[:max_samples]
 
+    #create empty array for stats and scores
+    stats = np.zeros( (len(data_selection), 2*numStats*polynomialDegree) ) #first 16*5 stats are polynimalized stats of home team, last 16*5 are visiting team
+    scores = np.zeros(len(data_selection))
+
+    #get minimum and maximum value for each stat (for standardization)
+    mins, maxs = findMinMaxFromStatSheet(open("data/mlbStats2021.txt").readlines())
+
+    #iterate through each game
+    for i in range(len(data_selection)):
+        game = data_selection[i]
+
+        #get information about the game
+        year = int(game.split(",")[0][:4])
+        homeTeam = game.split(",")[1]
+        visitingTeam = game.split(",")[2]
+        scoreDifference = float(game.split(",")[3])
+        gameWorthHome = float(game.split(",")[4])/gamesPerSeason
+        gameWorthVisitors = float(game.split(",")[4])/gamesPerSeason
+
+        #toggle variables for if the team's stats are found (1) or not (0)
+        home = 0
+        visiting = 0
+        
+        #get stat sheet from correct year
+        statSheet = open("data/mlbStats" + str(year) + ".txt").readlines()
+
+        #iterate through each row to find the teams we need
+        for j in range(len(statSheet)):
+            teamStats = statSheet[j]
+
+            #adding our game worth stat to stats array, this is temporary game worth should be a hyperparameter later (make the worth of this training sample less)
+            individualStats = teamStats.split(",")[3:]
+            individualStats.append(gameWorthHome)
+            individualStats.append(gameWorthVisitors)
+
+            #check for home team
+            if teamStats.split(",")[0] == homeTeam and home != 1:
+
+                home = 1
+                n = 0 #start position in stats array (first half is home team, second half is visiting team)
+
+                for s in range(numStats):
+                   #standardize stat using corresponding min and max
+                    stdStat = float(standardize(float(individualStats[s]), mins[s], maxs[s]))
+
+                    #polynomialize the stats
+                    for p in range(polynomialDegree):
+                        stats[i][n] = stdStat**p
+                        n += 1
+
+            elif teamStats.split(",")[0] == visitingTeam and visiting != 1:
+
+                visiting = 1
+                #second half of stats[i] is visiting team stats and their polynomials 
+                n = (numStats+1)*polynomialDegree
+
+                for s in range(numStats-1):
+                   #standardize stat using corresponding min and max
+                    stdStat = float(standardize(float(individualStats[s]), mins[s], maxs[s]))
+
+                    #polynomialize the stats
+                    for p in range(polynomialDegree):
+                        stats[i][n] = stdStat**p
+                        n += 1
+
+        # make sure we found the correct team's stats
+        if visiting == 0: print(str(year) + "incomplete stats: " + visitingTeam)
+        if home == 0: print(str(year) + " incomplete stats: " + homeTeam)
+
+        #save the outcome of the game
+        scores[i] = scoreDifference
+
+        #print completion status
+        if (i%300 == 0):
+            print("Processing games: %.2f" % float((i/len(data_selection)*100)) + "%")
+    return stats, scores
+
+
+#find minimums and maximums for each stat from a given stat sheet
+def findMinMaxFromStatSheet(s):
+    maxs = [0]*(numStats)
+    mins = [100000]*(numStats) #100000 so that all stats will be less than it
+
+    #iterate through the rows of stats
+    for i in range(len(s[1:])):
+        #seperate each row into individual stats
+        line = s[1:][i]
+        for j in range(len(line.split(",")[3:])):
+            #for each stat save new maximum and minimum value
+            maxs[j] = max(float(line.split(",")[3:][j]), maxs[j])
+            mins[j] = min(float(line.split(",")[3:][j]), mins[j])
+
+    return mins, maxs
+
+#creates a file containing the stat and score information with all the input data
+def writeToFile(x, y, filename):
+    file = open(filename, "w")
+    for i in range(len(x)): #for each game record
+        for j in range(len(x[i])): #for each stat
+            file.write(str(x[i][j]) + fieldSeperator) #add the stat and a seperator between fields
+        file.write(str(y[i]) + "\n") #add a newline after every entry
+        if i % 300 == 0:
+            print("Writing data to " + filename + " : %.2f" % float((i/len(x)*100)) + "%")
+
+    file.close()
+
+
+def standardize(stat, min, max):
+    return float(stat-min)/float(max-min) #scaling each feature between 0 and 1
+
+#returns stats and scores arrays when given the filename of a previously generated data set
+def loadFromFile(filename, max_entries=-6990):
+    if max_entries == -6990: max_entries = max_samples
+
+    try:
+        if max_entries < 0:
+            data = open(filename).readlines()
+        else:
+            data = open(filename).readlines()[:max_entries]
+    except:
+        print("Error: loadFromFile() input data file not found.")
+        return [], []
+        
+
+    #create empty arrays to fill
+    stats = np.zeros( (len(data), 2*numStats*polynomialDegree) ) #first 16*5 stats are polynimalized stats of home team, last 16*5 are visiting team
+    scores = np.zeros(len(data))
+    
+    #for each data entry
+    for i in range(len(data)):
+        #get the individual fields
+        fields = data[i].split(fieldSeperator)
+        for j in range(len(fields)):
+            if fields[j] != "":
+                #write them to the stats array except for the last field which goes to the scores array
+                if (j != len(stats[0])): stats[i][j] = float(fields[j])
+                else: scores[i] = float(fields[j])
+        #status update
+        if i % 300 == 0:
+            print("Reading data from " + filename + " : %.2f" % float((i/len(data)*100)) + "%")
+
+    return stats, scores
+
+#############################################################################################
+
+def generate():
+    #variable for the file name
+    gameData = ""
+
+    for y in range(endYear-startYear+1):
+        #getting data associated with current year
+        filename = "records/GL" + str(startYear+y) + ".TXT"
+        print("Reading in games: %.2f" % float(100.0*y/(endYear-startYear+1)) + "%")
+        #open data file and split into array of lines
+        infile = open(filename, 'r')
+        games = infile.read().split("\n")
+
+
+        for i in range(len(games)):
+            #divide line into different fields
+            cur = games[i].split(',')
+            
+            #some games have incomplete data or aren't data entires so we skip them
+            if len(cur) < 11: continue
+            
+            newData = ""
+
+            date = cur[0].strip('"') #get rid of the quotes around numbers and words
+            visitorScore = int( cur[9].strip('"') )
+            homeScore = int( cur[10].strip('"') )
+            homeTeam = teams.get( cur[6].strip('"'), "" )
+            visitingTeam = teams.get( cur[3].strip('"'), "" )
+            gamesPlayedHome = int(cur[8].strip('"'))
+            gamesPlayedVisitors = int(cur[5].strip('"'))
+
+            #make sure the data isn't incomplete then add to our gameData
+            if (date != "" and visitorScore != "" and homeScore != "" and homeTeam != "" and visitingTeam != "" and gamesPlayedHome != "" and gamesPlayedVisitors != ""):
+                newData += date + "," + homeTeam + "," + visitingTeam + "," + str(homeScore-visitorScore) + "," + str(gamesPlayedHome) + "," + str(gamesPlayedVisitors)
+                gameData += newData + "\n"
+        infile.close()
+
+    #generate txt file for all the game data
+    newFileName = os.getcwd() + "/data/ALLGAMES_" + str(startYear) + "-" + str(endYear) + ".txt"
+    os.remove(newFileName)
+    output = open(newFileName, "w")
+    #indicate which fields are which variables
+    output.write("date,homeTeam,visitingTeam,scoreDifference,gamesPlayedInSeason\n")
+    output.write(gameData)
+    output.close()
+
+    #get lines of the output data except for title and shuffle them randomly
+    lines = open(newFileName).readlines()[1:]
+    random.shuffle(lines)
+
+    #split our shuffled data into training, test, and validations data sets (50%, 25%, 25%)    
+    trainingSet = lines[:len(lines)//2] #write half of data to training
+    testSet = lines[3*len(lines)//4:] #write a quarter of data to test 
+    validationSet = lines[len(lines)//2:-len(lines)//4] #write a quarter of data to validation
+
+    # Next we convert our game data into an array of stats and score differences which we will save to a file to avoid having
+    # to compute it again later
+
+    trainingStats, trainingScores = createTrainingData(trainingSet)
+    testStats, testScores = createTrainingData(testSet)
+    validationStats, validationScores = createTrainingData(validationSet)
+
+
+    writeToFile(trainingStats, trainingScores, "data/TrainingData" + str(startYear) + "-" + str(endYear) + ".txt")
+    writeToFile(testStats, testScores, "data/TestData" + str(startYear) + "-" + str(endYear) + ".txt")
+    writeToFile(validationStats, validationScores, "data/ValidationData" + str(startYear) + "-" + str(endYear) + ".txt")
+
+    print("DONE")
+
+
+if len(sys.argv) > 1:
+    generate()
