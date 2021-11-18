6bd6af058a511da6eac63a45e1c097bf4f7b1ac6
antoinedang <antoine.dangeard@gmail.com>
2021-11-18 04:59:26 -0500

    It works now

--- BBModel.py
+++ BBModel.py
@@ -1,120 +1,227 @@
 import os, sys
+from types import TracebackType
 import numpy as np
 import pandas as pd
 import sklearn.linear_model as skm
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
+import sklearn.preprocessing as preprocessing
+from sklearn.decomposition import PCA
+from sklearn.neural_network import MLPRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.cross_decomposition import PLSRegression
+from datetime import datetime
 import GenerateGameData
+time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
-# hyperparameters
-learning_rate = 0.00001
-epsilon = 0.001
-lambda_value = 0.00001
-numStats = GenerateGameData.numStats
 
-def predictScore(w, s): # w is (2*numStats*polyDegree)x1 matrix, s is (2 * numStats * polyDegree)x1 matrix, output is array of predicted scores (one per game)
-    predictedScore = [0]*len(s)
-    for i in range(len(s)):
-        predictedScore[i] = s[i].dot(w)
-    return predictedScore
+print("Starting Baseball Stats Model > " + time)
 
-# function that calculates the gradient
-def calculate_regularized_grad(X_poly, y, W, lambda_value):
-    # let dW store dJ/dW
-    dW = np.zeros((len(W),1))
-    m = len(X_poly)
-    y_pred = np.matmul(X_poly, W)
+
+max_samples = -1
+use_pca = True
+use_scaling = True
+shuffle_data = True
+startYear = 1990
+endYear = 2020
+num_components = 500
+GenerateGameData.startYear = startYear
+GenerateGameData.endYear = endYear
+GenerateGameData.max_samples = max_samples
+
+
+def saveScore(accuracy, mse, name, parameters, startTime):
+    scores = open("modelResults.txt", 'a')
+    time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    output = ""
+    output += "-------------- " + startTime + " --------------\n"
+    output += "\t" + name + "\n"
+    output += "\t\t" + "Mean Squared Error: " + str(mse) + "\n"
+    output += "\t\t" + "Accuracy: " + str(accuracy) + "%\n"
+    output += "\t\t" + "Parameters: " + str(parameters)
+    output += "\n\t\t" + "Hyperparameters: num_components: " + str(num_components) + ", max_samples: " + str(max_samples) + ", startYear: " + str(startYear) + ", endYear: " + str(endYear)
+    output += "\n-------------- " + time + " --------------\n"
+
+    scores.write(output)
+    scores.close()
+
+def updateHighScore(accuracy, mse, name, parameters):
+    try:
+        data = open("bestMSE.txt", 'r').readlines()
+        lowestMSE = float(data[2][22:])
+    except:
+        data = [""]
+        lowestMSE = 1000000000.0
+
+    try:
+        data = open("bestAccuracy.txt", 'r').readlines()
+        highestAccuracy = float(data[3][12:18])
+    except:
+        data = [""]
+        highestAccuracy = 0.0
+
+
+    output = ""
+    if (accuracy > highestAccuracy):
+        output += "Highest Accuracy:\n"
+        output += "\t" + name + "\n"
+        output += "\t\t" + "Mean Squared Error: " + str(mse) + "\n"
+        output += "\t\t" + "Accuracy: " + str(accuracy) + "%\n"
+        output += "\t\t" + "Parameters: " + str(parameters)
+        output += "\n\t\t" + "Hyperparameters: num_components: " + str(num_components) + ", max_samples: " + str(max_samples) + ", startYear: " + str(startYear) + ", endYear: " + str(endYear)
+
+        bestAccuracy = open("bestAccuracy.txt", 'w')
+        bestAccuracy.write(output)
+        bestAccuracy.close()
     
-    error = y_pred - y
+    output = ""
+    if (mse < lowestMSE):
+        output += "Lowest Mean Squared Error:\n"
+        output += "\t" + name + "\n"
+        output += "\t\t" + "Mean Squared Error: " + str(mse) + "\n"
+        output += "\t\t" + "Accuracy: " + str(accuracy) + "%\n"
+        output += "\t\t" + "Parameters: " + str(parameters)
+        output += "\n\t\t" + "Hyperparameters: num_components: " + str(num_components) + ", max_samples: " + str(max_samples) + ", startYear: " + str(startYear) + ", endYear: " + str(endYear)
 
-    for j, w_j in enumerate(W):
-        ### YOUR CODE HERE - Calculate dW[j]
-        # Hint: You can just copy your implementation from Q2
-        # then append the L2 regularization term at the end
-        
-        dW[j] = (X_poly.T[j].dot(error))/m
-
-        if j != 0:
-          dW[j] += (lambda_value/m)*w_j
-        
-        ### ------------------------------
-        
-    return dW
-
-# function that caculates the change in W
-def calculate_dist(W_prev, W_cur):
-    print(W_cur)
-    return np.sqrt(np.sum((W_cur - W_prev)**2), dtype=np.float64)
-
-# use the above 2 functions to perform regularized gradient descent
-def train_regularized_polynomial_regression(X_poly, y, W, learning_rate, epsilon, lambda_value, verbose=True):
-  epoch_count = 0
-  while True:
-      #calculate current gradient
-      dW = calculate_regularized_grad(X_poly, y, W, lambda_value)
-
-      W_prev = W.copy()
-
-      for j in range(W.shape[0]): 
-        W[j] -= learning_rate*dW[j]
-
-      diff = calculate_dist(W_prev, W)
-      if (diff < epsilon):
-          break
-
-      epoch_count +=1
-
-      # print train error every 50 iterations
-      if verbose:
-        if epoch_count % 100 == 0:
-          y_train_pred = np.matmul(X_poly, W)
-          print('Training set Mean Squared Error: {}'.format(np.power((y_train_pred - y), 2).mean()))
-
-  print('Training complete.')
-  return W
+        bestMSE = open("bestMSE.txt", 'w')
+        bestMSE.write(output)
+        bestMSE.close()
 
 #function that prints the score of a model's prediction on a stat set
-def score(pred, true):
+def score(pred, true, name, parameters, reverse, startTime):
+    if (reverse): print (pred[:100])
     num_samples = len(pred)
     totalSquaredError = 0.0
-    correct_outcomes = 0.0
+    correct_outcomes = 0
     for i in range(len(pred)):
-        if pred[i]*true[i] > 0: correct_outcomes += 1
+        if reverse and pred[i]*true[i] < 0: correct_outcomes += 1
+        if pred[i]*true[i] > 0 and not reverse: correct_outcomes += 1
         totalSquaredError += (pred[i]-true[i])**2
 
-    print("===== SCORE =====")
+    updateHighScore(float(correct_outcomes/num_samples*100.0), float(totalSquaredError/num_samples), name, parameters)
+    saveScore(float(correct_outcomes/num_samples*100.0), float(totalSquaredError/num_samples), name, parameters, startTime)
+
+    print("===== " + name + " =====")
     print("Mean squared error: " + str(totalSquaredError/num_samples))
     print("Model predicted outcome correctly " + str(correct_outcomes) + " times out of " + str(num_samples) + " games. (%.2f" % float(correct_outcomes/num_samples*100.0) + "% Accuracy)")
-    print("=================")
 
 ################### TESTING CODE
 
-training_x, training_y = GenerateGameData.loadFromFile("data/TrainingData1990-2020.txt")
-test_x, test_y = GenerateGameData.loadFromFile("data/TestData1990-2020.txt")
+def start():
+    training_x, training_y, status = GenerateGameData.loadFromFile("data/TrainingData" + str(startYear) + "-" + str(endYear) + ".txt", shuffle_data)
+    if (status == 1):
+        GenerateGameData.setup()
+        training_x, training_y, status = GenerateGameData.loadFromFile("data/TrainingData" + str(startYear) + "-" + str(endYear) + ".txt", shuffle_data)
+    test_x, test_y, status = GenerateGameData.loadFromFile("data/TestData" + str(startYear) + "-" + str(endYear) + ".txt", shuffle_data)
 
-model = make_pipeline(StandardScaler(), skm.LinearRegression(n_jobs=-1))
-model.fit(training_x, training_y)
+    if (use_scaling):
+        scalerx = preprocessing.StandardScaler().fit(training_x)
+        training_x = scalerx.transform(training_x)
+        test_x = scalerx.transform(test_x)
 
-predictedScores = model.predict(test_x)
-score(predictedScores, test_y)
+        scalery = preprocessing.StandardScaler().fit(training_y.reshape(-1, 1))
+        training_y = scalery.transform(training_y.reshape(-1, 1))
+        test_y = scalery.transform(test_y.reshape(-1, 1))
+
+    if (use_pca):
+        pca = PCA(n_components = num_components)
+        old_training_x = training_x
+        old_test_x = test_x
+        training_x = pca.fit_transform(training_x)
+        test_x = pca.transform(test_x)
+
+    #MODELS
+    #custom parameters
+    hyperparameters = [ [0], [0], [0], [0] ]
+    hyperparameters[0] = [ 50, 75, 100, 200, 500, 750, 1000, 1500, 2500 ] #num_components for pca
+
+    hyperparameters[1] = [ 0.5, 1.0, 1.5, 2.0, 5.0 ] #for alpha
+    hyperparameters[2] = [ True, False ] #for warm start
+    hyperparameters[3] = [ 10, 20, 30, 40, 50, 100, 200 ] #for leaf size and n_neighbours and batch size
+
+    #runDefaultModels(training_x, training_y, test_x, test_y)
+
+    for i in hyperparameters[0]:#num_components for pca
+        pca = PCA(n_components = i)
+        training_x = pca.fit_transform(old_training_x)
+        test_x = pca.transform(old_test_x)
+
+        
+        startTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        model = skm.LinearRegression(fit_intercept=True, normalize='deprecated', copy_X=True, n_jobs=-1, positive=False)
+        model.fit(training_x, training_y)
+        predictedScores = model.predict(test_x)
+        score(predictedScores, test_y, "Linear", model.get_params(), False, startTime)
+
+        startTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        model = PLSRegression(n_components=i, scale=False, max_iter=10000, tol=1e-06, copy=True)
+        model.fit(training_x, training_y)
+        predictedScores = model.predict(test_x)
+        score(predictedScores, test_y, "Partial Least Squares", model.get_params(), False, startTime)
+
+        for alpha in hyperparameters[1]: #for alpha
+
+            startTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+            model = skm.Ridge(alpha=alpha, max_iter=None, tol=0.00001, solver='auto')
+            model.fit(training_x, training_y)
+            predictedScores = model.predict(test_x)
+            score(predictedScores, test_y, "Ridge", model.get_params(), False, startTime)
+
+            for warmstart in hyperparameters[2]:#for warm start
+                startTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+                model = skm.Lasso(alpha=alpha, fit_intercept=False, normalize='deprecated', precompute=False, copy_X=True, max_iter=10000, tol=0.0001, warm_start=warmstart, positive=False, random_state=None, selection='random')
+                model.fit(np.asfortranarray(training_x), training_y)
+                predictedScores = model.predict(test_x)
+                score(predictedScores, test_y, "Lasso", model.get_params(), True, startTime)
+
+        for j in hyperparameters[3]:#for n_neighbours and batch size
+
+            for leaf in hyperparameters[3]:#for leaf size
+
+                startTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+                model = KNeighborsRegressor(n_neighbors=j, weights='distance', algorithm='auto', leaf_size=leaf, p=2, metric='minkowski', metric_params=None, n_jobs=-1)
+                model.fit(training_x, training_y)
+                predictedScores = model.predict(test_x)
+                score(predictedScores, test_y, "K-Nearest Neighbours", model.get_params(), False, startTime)
 
 
-exit()
+def runDefaultModels(trainx, trainy, testx, testy):
+    
+    startTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    model = skm.LinearRegression(n_jobs=-1)
+    model.fit(trainx, trainy)
+    predictedScores = model.predict(testx)
+    score(predictedScores, testy, "Linear", model.get_params(), False, startTime)
 
+    startTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    model = skm.Ridge(alpha=1.0, max_iter=None, tol=0.0001, solver='auto')
+    model.fit(trainx, trainy)
+    predictedScores = model.predict(testx)
+    score(predictedScores, testy, "Ridge", model.get_params(), False, startTime)
 
-valid_x, valid_y = getTrainingData(valid_set)
-W = train_regularized_polynomial_regression(training_x, training_y, weights, learning_rate, epsilon, lambda_value)
-#calculate squared error on training set
-pred_scores = predictScore(training_x, W)
-mse = np.power((pred_scores - training_y), 2).mean()
-print('\nTraining Mean Squared Error: {}'.format(mse))
+    startTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    model = skm.Lasso(alpha=1.0, fit_intercept=True, normalize='deprecated', precompute=False, copy_X=True, max_iter=1000, tol=0.0001, warm_start=False, positive=False, random_state=None, selection='cyclic')
+    model.fit(np.asfortranarray(trainx), trainy)
+    predictedScores = model.predict(testx)
+    score(predictedScores, testy, "Lasso", model.get_params(), False, startTime)
 
+    startTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    model = PLSRegression(n_components=2, scale=True, max_iter=500, tol=1e-06, copy=True)
+    model.fit(trainx, trainy)
+    predictedScores = model.predict(testx)
+    score(predictedScores, testy, "Partial Least Squares", model.get_params(), False, startTime)
 
-#calculate squared error on test set
-pred_scores = predictScore(test_x, W)
-mse = np.power((pred_scores - test_y), 2).mean()
-print('\nTest Mean Squared Error: {}'.format(mse))
-#calculate squared error on test set
-pred_scores = predictScore(valid_x, W)
-mse = np.power((pred_scores - valid_y), 2).mean()
-print('\Validation Mean Squared Error: {}'.format(mse))
+    startTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    model = SVR(kernel='rbf', degree=3, gamma='scale', coef0=0.0, tol=0.001, C=1.0, epsilon=0.1, shrinking=True, cache_size=200, max_iter=- 1, verbose=True)
+    model.fit(trainx, trainy)
+    predictedScores = model.predict(testx)
+    score(predictedScores, testy, "Support Vector Machines", model.get_params(), False, startTime)
+
+    startTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    model = RandomForestRegressor(n_estimators=100, criterion='squared_error', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, min_impurity_decrease=0.0, bootstrap=True, oob_score=False, n_jobs=-1, random_state=None, warm_start=False, ccp_alpha=0.0, max_samples=None, verbose=True)
+    model.fit(trainx, trainy)
+    predictedScores = model.predict(testx)
+    score(predictedScores, testy, "Random Forest", model.get_params(), False, startTime)
+
+start()
